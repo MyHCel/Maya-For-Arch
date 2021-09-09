68cb145aad43c3f0699496ab87a7c0d0db9deecf
MyHCel <hectorcelis6@gmail.com>
2021-09-09 14:20:13 -0500

    Fixed a typo

--- scripts/2020/dependencies.sh
+++ scripts/2020/dependencies.sh
@@ -2,4 +2,4 @@
 
 # ====== Update and Install dependencies ======
 
-yay -Syu ibjpeg lib32-libjpeg libjpeg6 audiofile xorg-fonts-misc libxp python2 python2-backports ld-lsb lsb-release cpio xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts gsfonts adobe-source-code-pro-fonts xorg-xlsfonts xorg-fonts-type1 libtiff libpng15 ncurses5-compat-libs lib32-ncurses5-compat-libs libffi6
+yay -Syu libjpeg lib32-libjpeg libjpeg6 audiofile xorg-fonts-misc libxp python2 python2-backports ld-lsb lsb-release cpio xorg-fonts-100dpi xorg-fonts-75dpi xorg-fonts gsfonts adobe-source-code-pro-fonts xorg-xlsfonts xorg-fonts-type1 libtiff libpng15 ncurses5-compat-libs lib32-ncurses5-compat-libs libffi6
